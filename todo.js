95d99471655c7832cbffed6d3f948efba2c4bc5a
Olumide Ogundele <olumideralph@gmail.com>
2020-01-18 17:34:08 +0100

    implement more features for the todo app

--- todo.js
+++ todo.js
@@ -26,6 +26,8 @@ function addTodo() {
         todoList.sort();
         // update the todo list
         updateTodoList();
+        // filter the tod list
+        filterTodoList();
     }
 }
 var todoListDiv = document.getElementById("todoListContainer");
@@ -40,7 +42,40 @@ function updateTodoList() {
     todoList.forEach(function (item) {
         var li = document.createElement("li");
         li.setAttribute("class", "todo-list-item");
-        li.innerText = item;
+        li.innerHTML = "<a href=\"#\" onclick='removeTodoListItem(\"" + item + "\")'>" + item + "</a>";
         ul.appendChild(li);
     });
+}
+function filterTodoList() {
+    console.log("Filtering the rendered list..");
+    var todoListHtml = document.getElementById("todoList");
+    if (todoListHtml === null) {
+        console.log("Nothing to Filter");
+        return;
+    }
+    var todoListFilter = document.getElementById("todoFilter");
+    var todoListFilterText = todoListFilter.value.toUpperCase();
+    todoListHtml.childNodes.forEach(function (item) {
+        var itemText = item.textContent;
+        if (itemText !== null) {
+            itemText = itemText.toUpperCase();
+            if (itemText.startsWith(todoListFilterText)) {
+                item.style.display = "list-item";
+            }
+            else {
+                item.style.display = "none";
+            }
+        }
+    });
+}
+function removeTodoListItem(itemToRemove) {
+    console.log("Item to remove: ", itemToRemove);
+    todoList = todoList.filter(function (value, _index, _array) {
+        if (value === itemToRemove) {
+            return false;
+        }
+        return true;
+    });
+    updateTodoList();
+    filterTodoList();
 }
--- todo.ts
+++ todo.ts
@@ -1,4 +1,4 @@
-const todoList: string[] = [];
+let todoList: string[] = [];
 const todoInput: HTMLInputElement = document.getElementById("todoInput") as HTMLInputElement;
 
 // addTodo - takes user input and create a new todo
@@ -32,6 +32,9 @@ function addTodo(): void {
 
         // update the todo list
         updateTodoList();
+
+        // filter the tod list
+        filterTodoList();
     }
 }
 
@@ -50,7 +53,49 @@ function updateTodoList(): void {
     todoList.forEach(item => {
         const li = document.createElement("li");
         li.setAttribute("class", "todo-list-item");
-        li.innerText = item;
+        li.innerHTML = `<a href="#" onclick='removeTodoListItem("${item}")'>${item}</a>`;
         ul.appendChild(li);
     });
+}
+
+function filterTodoList() :void {
+    console.log("Filtering the rendered list..");
+
+    const todoListHtml: HTMLUListElement = document.getElementById("todoList") as HTMLUListElement;
+
+    if (todoListHtml === null) {
+        console.log("Nothing to Filter");
+        return;
+    }
+
+    const todoListFilter = document.getElementById("todoFilter") as HTMLInputElement;
+    const todoListFilterText = todoListFilter.value.toUpperCase();
+
+    todoListHtml.childNodes.forEach((item) => {
+        let itemText: string | null = item.textContent;
+
+        if (itemText !== null) {
+            itemText = itemText.toUpperCase();
+
+            if (itemText.startsWith(todoListFilterText)) {
+                (item as HTMLLIElement).style.display = "list-item";
+            } else {
+                (item as HTMLLIElement).style.display = "none";
+            }
+        }
+    });
+}
+
+function removeTodoListItem(itemToRemove: string): void {
+    console.log("Item to remove: ", itemToRemove);
+
+    todoList = todoList.filter((value: string, _index, _array) => {
+        if (value === itemToRemove) {
+            return false;
+        }
+        return true;
+    });
+
+    updateTodoList();
+    filterTodoList();
 }
